2e165b218659f9181e071454bf406a7719860aa2
williansaio <williansaio23@gmail.com>
2019-08-09 10:50:37 +0900

    [Refactor]ちょいコントローラー周りをリファクタ

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,20 +1,25 @@
 class LineBotController < ApplicationController
 
   def callback
-    set_line_api
-    if @line_api.valid?
-      @line_api.send
+    if line_api.valid?
+      line_api.send
       head :ok
     else
-      render status: 400
+      render status: :bad_request
     end
   end
 
   private
 
-  def set_line_api
-    request_body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    @line_api = LineApi.new(ENV["LINE_CHANNEL_SECRET"], ENV["LINE_CHANNEL_TOKEN"], request_body, signature)
+  def request_body
+    request.body.read
+  end
+
+  def signature
+    request.env['HTTP_X_LINE_SIGNATURE']
+  end
+
+  def line_api
+    LineApi.new(ENV["LINE_CHANNEL_SECRET"], ENV["LINE_CHANNEL_TOKEN"], request_body, signature)
   end
 end
